78bd1ef6cc08b8c8ab9b2f9aa0e4aff3461d295a
Thomas Rose <mail@tom-rose.de>
2023-01-10 12:17:41 +0000

    add php doc correctly

--- htdocs/wp-content/plugins/wp-codespace/wp-codespace.php
+++ htdocs/wp-content/plugins/wp-codespace/wp-codespace.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * This plugin is just for demonstrating.
+ *
+ * @package WP_Codespace
+ * @version 1.0.0
+ */
+
 /*
 Plugin Name: WP Codespace
 Description: This plugin is just for demonstrating.
