392890a6b7e2543c808a8e326d8b43954b3961dc
Thomas Rose <mail@tom-rose.de>
2023-01-10 12:24:10 +0000

    add example func

--- htdocs/wp-content/plugins/wp-codespace/wp-codespace.php
+++ htdocs/wp-content/plugins/wp-codespace/wp-codespace.php
@@ -11,3 +11,12 @@ Plugin Name: WP Codespace
 Description: This plugin is just for demonstrating.
 Author: Tom Rose
 */
+
+
+add_action( 'admin_notices', 'wp_codespace_notices' );
+/**
+ * Adds a Hello Codespace to the admin area.
+ */
+function wp_codespace_notices() {
+	echo '<p>Hello codespace!</p>';
+}
