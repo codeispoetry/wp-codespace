1de20a55db2bd4a2f4c5eba0e1ce55995e1aaba8
Thomas Rose <mail@tom-rose.de>
2023-01-15 16:57:09 +0000

    add democontent

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,22 +3,30 @@
 # Apache
 sudo chmod 777 /etc/apache2/sites-available/000-default.conf
 sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf > /etc/apache2/sites-available/000-default.conf
-
 update-rc.d apache2 defaults 
 service apache2 start
 
-# WordPress
-wp core download --locale=de_DE --path=wordpress
+LOCALE="de_DE"
+
+# WordPress Cire install
+wp core download --locale=$LOCALE --path=wordpress
 cd wordpress
 wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
-
 LINE_NUMBER=`grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1`
 sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config-addendum.txt" wp-config.php && sed -i -e "s/CODESPACE_NAME/$CODESPACE_NAME/g"  wp-config.php
-
 wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
+
+# Selected plugins
 wp plugin delete akismet
 wp plugin install show-current-template --activate
 wp plugin activate wp-codespace
+
+# Demo content for WordPress
+wp plugin install wordpress-importer --activate
+curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml > demo-content.xml
+wp import demo-content.xml--authors=create
+rm demo-content.xml
+
 cd ..
 
 #Xdebug
