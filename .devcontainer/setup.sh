f7b42ee69de01e370f146036129b5d2d2566b59b
araeubig <129953354+araeubig@users.noreply.github.com>
2023-04-08 18:41:52 +0200

    Update and rename postCreateCommand.sh to setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,6 +10,7 @@ service apache2 start
 LOCALE="de_DE"
 
 # WordPress Core install
+echo "Installing WordPress..."
 wp core download --locale=$LOCALE --path=wordpress
 cd wordpress
 wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
@@ -18,11 +19,17 @@ sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config-addendum.txt" wp-config.php &
 wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
 
 # Selected plugins
+echo "Installing WordPress Plugins, Themes..."
 wp plugin delete akismet
-wp plugin install show-current-template --activate
+wp plugin delete hello
+wp plugin install query-monitor --activate
+wp plugin install transients-manager --activate
+wp plugin install loco-translate --activate
+# wp plugin install show-current-template --activate
 wp plugin activate wp-codespace
 
 # Demo content for WordPress
+echo "Installing WordPress Theme Test Data..."
 wp plugin install wordpress-importer --activate
 curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml > demo-content.xml
 wp import demo-content.xml --authors=create
@@ -32,6 +39,7 @@ rm demo-content.xml
 echo xdebug.log_level=0 | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
 # install dependencies
+echo "Installing dependencies..."
 cd $REPO_FOLDER
 npm install 
 composer install
